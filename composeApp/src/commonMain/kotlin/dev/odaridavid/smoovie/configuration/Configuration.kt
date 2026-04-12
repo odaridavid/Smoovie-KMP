367ba71a82e561a97526d381f55523c80fa440fb
Odari <david.kiribwa@blacklane.com>
2026-04-13 00:15:49 +0200

    build image links

--- composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/configuration/Configuration.kt
+++ composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/configuration/Configuration.kt
@@ -12,6 +12,26 @@ data class Configuration(
 data class ImagesConfiguration(
     @SerialName("base_url") val baseUrl: String,
     @SerialName("secure_base_url") val secureBaseUrl: String,
-    @SerialName("backdrop_sizes") val backdropSizes: List<String>,
-    @SerialName("poster_sizes") val posterSizes: List<String>,
-)
+) {
+}
+
+enum class BackdropSize(
+    val apiValue: String,
+) {
+    SMALL("w300"),
+    MEDIUM("w780"),
+    LARGE("w1280"),
+    ORIGINAL("original"),
+}
+
+enum class PosterSize(
+    val apiValue: String,
+) {
+    TINY("w92"),
+    SMALL("w154"),
+    COMPACT("w185"),
+    MEDIUM("w342"),
+    LARGE("w500"),
+    XLARGE("w780"),
+    ORIGINAL("original"),
+}
--- composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/configuration/ConfigurationStore.kt
+++ composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/configuration/ConfigurationStore.kt
@@ -4,10 +4,19 @@ import org.koin.core.annotation.Single
 
 @Single
 class ConfigurationStore {
-    var imagesConfiguration: ImagesConfiguration? = null
-        private set
+    private var imagesConfiguration: ImagesConfiguration? = null
 
     fun save(config: ImagesConfiguration) {
         imagesConfiguration = config
     }
+
+    fun backdropUrl(
+        path: String?,
+        size: BackdropSize = BackdropSize.MEDIUM,
+    ): String? = path?.let { "${imagesConfiguration?.secureBaseUrl}${size.apiValue}$it" }
+
+    fun posterUrl(
+        path: String?,
+        size: PosterSize = PosterSize.MEDIUM,
+    ): String? = path?.let { "${imagesConfiguration?.secureBaseUrl}${size.apiValue}$it" }
 }

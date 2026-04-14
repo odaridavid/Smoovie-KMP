4a78eff8ec7e292e51361c6d131582911bb9d52a
Odari <david.kiribwa@blacklane.com>
2026-04-15 01:26:58 +0200

    fix deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,7 +37,7 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
-            implementation(compose.materialIconsExtended)
+            implementation(libs.material.icons.extended)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)

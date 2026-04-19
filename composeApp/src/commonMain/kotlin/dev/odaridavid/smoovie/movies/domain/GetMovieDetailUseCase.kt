e1ab3a948722eb5c49e2bdc098c63d77a0ba16e9
Odari <david.kiribwa@blacklane.com>
2026-04-19 19:26:43 +0200

    Create moview detail use case

---
+++ composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/movies/domain/GetMovieDetailUseCase.kt
@@ -0,0 +1,20 @@
+package dev.odaridavid.smoovie.movies.domain
+
+import dev.odaridavid.smoovie.configuration.BackdropSize
+import dev.odaridavid.smoovie.configuration.ConfigurationStore
+import dev.odaridavid.smoovie.movies.MovieDetailUiModel
+import dev.odaridavid.smoovie.movies.toDetailUiModel
+
+class GetMovieDetailUseCase(
+    private val repository: MoviesRepository,
+    private val configurationStore: ConfigurationStore,
+) {
+    suspend operator fun invoke(movieId: Int): MovieDetailUiModel =
+        repository.getMovieDetail(movieId).let { detail ->
+            detail.toDetailUiModel(
+                backdropUrl = configurationStore.backdropUrl(detail.backdropPath, BackdropSize.LARGE),
+                posterUrl = configurationStore.posterUrl(detail.posterPath),
+                profileUrlResolver = { configurationStore.profileUrl(it) },
+            )
+        }
+}

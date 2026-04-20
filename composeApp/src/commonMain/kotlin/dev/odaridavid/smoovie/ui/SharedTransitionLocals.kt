f528995925c2563fa60dffefd8d9e0d19b7c2798
Odari <david.kiribwa@blacklane.com>
2026-04-20 21:18:11 +0200

    add missing file

---
+++ composeApp/src/commonMain/kotlin/dev/odaridavid/smoovie/ui/SharedTransitionLocals.kt
@@ -0,0 +1,8 @@
+package dev.odaridavid.smoovie.ui
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.runtime.compositionLocalOf
+
+val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope?> { null }
+val LocalAnimatedVisibilityScope = compositionLocalOf<AnimatedVisibilityScope?> { null }

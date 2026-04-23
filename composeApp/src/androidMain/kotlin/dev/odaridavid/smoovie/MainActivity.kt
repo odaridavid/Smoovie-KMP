b8103aa0f0e48811277bcbbfbdbece360b745440
Odari <david.kiribwa@blacklane.com>
2026-04-23 09:28:16 +0200

    keep status bar icons white

--- composeApp/src/androidMain/kotlin/dev/odaridavid/smoovie/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/odaridavid/smoovie/MainActivity.kt
@@ -1,13 +1,17 @@
 package dev.odaridavid.smoovie
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+        )
         super.onCreate(savedInstanceState)
         setContent {
             App()
